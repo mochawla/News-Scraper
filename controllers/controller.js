f514b645dbf1f217eafd2e22598d1853a9f36f83
Mohammed Chawla <chawla.mohammed@gmail.com>
2018-09-10 04:06:35 -0500

    getting scrape data from website now , need to finish routes, work on ajax calls and js in app.js file, and create handlebars for the front-end

--- controllers/controller.js
+++ controllers/controller.js
@@ -19,13 +19,13 @@ app.get("/", function(req, res) {
 //a get route that will scrape for articles from a website (includes query to database to get all articles)*
     app.get("/scrape", function(req, res){
         
-        axios.get("https://www.nytimes.com//").then(function(response){
+        axios.get("https://news.ycombinator.com/").then(function(response){
 
         // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
             //console.log(response.data)
             // Now, we grab every h2 within an article tag, and do the following:
-            $("article h2").each(function(i, element) {
+            $(".title").each(function(i, element) {
                 // Save an empty result object
                 var result = {};
 
@@ -36,15 +36,15 @@ app.get("/", function(req, res) {
                 result.link = $(this)
                 .children("a")
                 .attr("href");
-                result.summary = $(this)
-                .children(".summary")
-                .text();
-                console.log(result)
+                // result.summary = $(this)
+                // .children(".summary")
+                // .text();
+                //console.log(result)
                 // Create a new Article using the `result` object built from scraping
                 db.Article.create(result)
                 .then(function(dbArticle) {
                     // View the added result in the console
-                    //console.log(dbArticle);
+                    console.log(dbArticle);
                 });
                 // .catch(function(err) {
                 // // If an error occurred, send it to the client
@@ -61,7 +61,19 @@ app.get("/", function(req, res) {
     });
 
 
-//a get route for viewing articles that have been scraped/getting them from the db (could do it on homepage instead)*
+//a get route for viewing articles that have been scraped/getting them from the db*
+    app.get("/articles", function(req, res) {
+        // Grab every document in the Articles collection
+        db.Article.find({})
+        .then(function(dbArticle) {
+    // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+    // If an error occurred, send it to the client
+        res.json(err);
+    });
+})
 
 //a post route that will save an article (will update the saved boolean in the db from false to true based on id)
 
@@ -71,7 +83,7 @@ app.get("/", function(req, res) {
 
 //a get route that will grab an article by it's id and populate it with it's note*
 
-//a post route that will creating/saving/updating an articles note*
+//a post route that will create/save/update an articles note*
 
 //a route that will delete a note by id
 
